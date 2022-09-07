cb6787006a224a3f2344e82fef15971ec3bc4c95
Paul Oloyede <52790286+paulayo93@users.noreply.github.com>
2022-09-07 16:31:33 +0100

    slide toggle init repo

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Animated, // import the animated libriary
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
+  const translateAnimate = useRef(new Animated.Value(0)).current;
+  const [isToggle, setIsToggle] = useState(false);
+
+  const animateInterpolate = translateAnimate.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 46],
+    extrapolate: "clamp",
+  });
+
+  const animateElement = () => {
+    const toValue = isToggle ? 0 : 1;
+
+    Animated.timing(translateAnimate, {
+      toValue,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => {
+      setIsToggle(!isToggle);
+    });
+  };
+
+  const animationStyle = {
+    transform: [
+      {
+        translateX: animateInterpolate,
+      },
+    ],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/home.jpg")}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={() => animateElement()}>
+        <View style={styles.toggleContainer}>
+          <Animated.View
+            style={[styles.toggle, animationStyle]}
+          ></Animated.View>
+        </View>
+      </TouchableWithoutFeedback>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  toggleContainer: {
+    borderRadius: 20,
+    borderWidth: 0,
+    height: 35,
+    width: 80,
+    backgroundColor: "grey",
+    justifyContent: "center",
+    paddingHorizontal: 2
+  },
+  toggle: {
+    backgroundColor: "blue",
+    height: 30,
+    width: 30,
+    borderRadius: 16,
+    borderWidth: 0,
   },
 });
